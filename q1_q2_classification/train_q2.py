c9d94912a3b73ed5ca323c4bb8c90316b06e33b5
Stevenshi12 <stevenshi1126@gmail.com>
2023-09-26 16:19:32 -0400

    train q2 file

---
+++ q1_q2_classification/train_q2.py
@@ -0,0 +1,87 @@
+import torch
+import trainer
+from utils import ARGS
+from simple_cnn import SimpleCNN
+from voc_dataset import VOCDataset
+import numpy as np
+import torchvision
+import torch.nn as nn
+import random
+
+
+class ResNet(nn.Module):
+    def __init__(self, num_classes) -> None:
+        super().__init__()
+
+        self.resnet = torchvision.models.resnet18(weights='IMAGENET1K_V1')
+        # for params in self.resnet.parameters():
+        #     params.requires_grad = False
+        ##################################################################
+        # TODO: Define a FC layer here to process the features
+        ##################################################################
+        # self.gelu = torch.nn.GeLU()
+        self.drop = torch.nn.Dropout(p=0.2)
+        self.linear = torch.nn.Linear(1000,20)
+        ##################################################################
+        #                          END OF YOUR CODE                      #
+        ##################################################################
+        
+    def forward(self, x):
+        ##################################################################
+        # TODO: Return unnormalized log-probabilities here
+        ##################################################################
+        x = self.resnet(x)
+        out = self.linear(self.drop(x))
+        return out
+        ##################################################################
+        #                          END OF YOUR CODE                      #
+        ##################################################################
+
+
+if __name__ == "__main__":
+    np.random.seed(0)
+    torch.manual_seed(0)
+    random.seed(0)
+
+    ##################################################################
+    # TODO: Create hyperparameter argument class
+    # We will use a size of 224x224 for the rest of the questions. 
+    # Note that you might have to change the augmentations
+    # You should experiment and choose the correct hyperparameters
+    # You should get a map of around 50 in 50 epochs
+    ##################################################################
+    args = ARGS(
+        epochs=50,
+        inp_size=224,
+        use_cuda=True,
+        val_every=70,
+        lr = 0.0004,
+        batch_size=100,
+        step_size=10,
+        gamma=0.4#TODO
+    )
+    ##################################################################
+    #                          END OF YOUR CODE                      #
+    ##################################################################
+    
+    print(args)
+
+    ##################################################################
+    # TODO: Define a ResNet-18 model (https://arxiv.org/pdf/1512.03385.pdf) 
+    # Initialize this model with ImageNet pre-trained weights
+    # (except the last layer). You are free to use torchvision.models 
+    ##################################################################
+
+    model = ResNet(len(VOCDataset.CLASS_NAMES)).to(args.device)
+
+    ##model.load("checkpoint-resnet_5-epoch15.pth")
+    ##################################################################
+    #                          END OF YOUR CODE                      #
+    ##################################################################
+
+    # initializes Adam optimizer and simple StepLR scheduler
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=args.step_size, gamma=args.gamma)
+    # trains model using your training code and reports test map
+    test_ap, test_map = trainer.train(args, model, optimizer, scheduler)
+    print('test map:', test_map)
